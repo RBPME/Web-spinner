3c5a43b9eeac2f1b635c9df27f2bbf0b6cf44c52
RBPME <rasm245r@gmail.com>
2022-03-15 13:57:49 +0100

    wrote base html

--- Login.php
+++ Login.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <script src="/JS/password.js">
-    </script>
+        <script src="/JS/password.js"></script>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Web-Spinner</title>
     </head>
     <body onload="variables()">
         <div class="formdiv">
--- signup.php
+++ signup.php
@@ -2,9 +2,10 @@
 <!-- html for udsene-->
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Web-Spinner</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
 
 
