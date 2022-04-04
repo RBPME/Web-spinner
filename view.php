9a3421a24120f45a3650852f5cd9acfb6a25aa34
RBPME <rasm245r@gmail.com>
2022-04-04 15:08:02 +0200

    made comment

--- view.php
+++ view.php
@@ -10,6 +10,7 @@
 
     <?php
         require_once "./SQL/DB_handleling/connect.php"; 
+        session_start();
         $id = $_GET['id'];
         $result = preformQuery("SELECT * FROM content WHERE id = ".$id."");
         $row = mysqli_fetch_array($result);
@@ -22,6 +23,35 @@
             echo '<br><br>';
             echo "<img src='https://rasm245r.elev.vtg.dk/".$row['imgpath']."' width='100px'>";
         
+        ?>
+
+        <hr>
+        <br>
+        <h2>Kommenter</h2>
+        <form method="post" action="#">
+            <textarea name="comm" id="" cols="30" rows="10"></textarea> <br>
+            <input type="submit">
+        </form>
+
+        <?php 
+            if ($_SESSION["comm"] != $_POST["comm"]) {
+                $_SESSION["comm"] = $_POST["comm"];
+                $comment = $_POST["comm"];
+                preformQuery("INSERT INTO comment (parentId, comment) VALUES ('$id', '$comment')");
+
+                
+            }
+        ?>
+        <hr>
+        <?php 
+            $result = preformQuery("SELECT * FROM comment WHERE parentId = ".$id."");
+            while ($row = mysqli_fetch_array($result)) {
+
+                echo str_replace("\n", "<br>", $row['comment']);
+                echo "<hr>";
+                
+            }
+
         ?>
 
     </body>
